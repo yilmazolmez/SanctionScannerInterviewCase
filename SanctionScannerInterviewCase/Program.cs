0a850113f86133de54cdd3bd7169cb1a1471dcca
YILMAZ OLMEZ <yilmaz.olmez@affinitybox.com>
2022-07-30 17:03:01 +0300

    HtmlAgilityPack nuget package added

--- SanctionScannerInterviewCase/Program.cs
+++ SanctionScannerInterviewCase/Program.cs
@@ -1,4 +1,5 @@
-﻿using SanctionScannerInterviewCase.Model;
+﻿using HtmlAgilityPack;
+using SanctionScannerInterviewCase.Model;
 using SanctionScannerInterviewCase.Utilities;
 using System;
 using System.Collections.Generic;
@@ -9,17 +10,28 @@ namespace SanctionScannerInterviewCase
     {
         static void Main(string[] args)
         {
-            List<PostModel> PostModelList = new List<PostModel>();
-
-            var siteContent = HttpWebRequestService.RequestWithUrl("https://www.otokocikinciel.comasdasdsa");
-
-            if (siteContent == null  || siteContent == "")
+            try
             {
-                Console.WriteLine("Empty content");
-                return;
+                List<PostModel> PostModelList = new List<PostModel>();
+
+                var siteContent = HttpWebRequestService.RequestWithUrl("https://www.otokocikinciel.com");
+
+                if (siteContent == null || siteContent == "")
+                {
+                    Console.WriteLine("Empty content");
+                    return;
+                }
+
+                HtmlDocument pageDocument = new HtmlDocument();
+                pageDocument.LoadHtml(siteContent);
+
+
+
             }
-
-
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in Main. {ex.Message}");
+            }
         }
     }
 }
